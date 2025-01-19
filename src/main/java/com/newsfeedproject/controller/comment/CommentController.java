63b04eae60123dfa210393a4ec10b0c3ca14f9ee
answerin1 <dabin990723@naver.com>
2025-01-20 08:59:35 +0900

    fix: CommentController 기능 구현 수정 댓글 및 대댓글 관련 로직 수정

--- src/main/java/com/newsfeedproject/controller/comment/CommentController.java
+++ src/main/java/com/newsfeedproject/controller/comment/CommentController.java
@@ -23,7 +23,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/posts/{post_id}//comments")
+@RequestMapping("/api/posts/{post_id}/comments")
 public class CommentController {
 
 	// 속성
@@ -36,27 +36,33 @@ public class CommentController {
 	public ResponseEntity<CreateCommentResponseDto> createComment(
 		@PathVariable("post_id") Long postId, // (name="...")로 적으면 더 빡세게 적을 수 있다!
 		@RequestBody CreateCommentRequestDto createRequestDto
-	) {
+	) { //+ session.getAttribute~~ 인증 시 추가
 		CreateCommentResponseDto createdComment = commentService.createComment(postId, createRequestDto);
 		return new ResponseEntity<>(createdComment, HttpStatus.CREATED);
 	}
 
-	@GetMapping // 댓글(대댓글 포함) 다건 조회
-	public ResponseEntity<List<CreateCommentResponseDto>> findAllComments(
+	@GetMapping // 댓글 다건 조회
+	public ResponseEntity<List<CreateCommentResponseDto>> findParentComments(
 		@PathVariable("post_id") Long postId
 	) {
 		List<CreateCommentResponseDto> createCommentResponseDtoList = commentService.findAllComments(postId);
 		return new ResponseEntity<>(createCommentResponseDtoList, HttpStatus.OK);
 	}
 
-	// @GetMapping
-	// @PathVariable("/{comment_id}/replies") //findChildrenComments 로 로직을 짜보는 어떨지?
+	@GetMapping("/{comment_id}/replies")  // 대댓글만 다건 조회
+	public ResponseEntity<List<CreateCommentResponseDto>> findChildrenComments(
+		@PathVariable("comment_id") Long commentId // 여기서의 commentId = parentId
+	) {
+		// 부모 댓글의 대댓글(답글) 조회
+		List<CreateCommentResponseDto> replies = commentService.findChildrenComments(commentId);
+		return new ResponseEntity<>(replies, HttpStatus.OK);
+	}
 
 	@PutMapping("/{comment_id}") // 댓글 수정
 	public ResponseEntity<UpdateCommentResponseDto> updateComment(
 		@PathVariable("comment_id") Long commentId,
 		@RequestBody UpdateCommentRequestDto updateCommentRequestDto
-	) {
+	) { //+ session.getAttribute~~ 인증 시 추가
 		UpdateCommentResponseDto updatedComment = commentService.updateComment(commentId, updateCommentRequestDto);
 		return new ResponseEntity<>(updatedComment, HttpStatus.OK);
 	}
@@ -64,7 +70,7 @@ public class CommentController {
 	@DeleteMapping("/{comment_id}") // 댓글 삭제
 	public ResponseEntity<Void> deleteComment( // void라고 쓰는게 맞나? DeletRe~Dto를 만들어서..메세지 출력할 수 있도록
 		@PathVariable("comment_id") Long commentId
-	) {
+	) { //+ session.getAttribute~~ 인증 시 추가
 		commentService.deleteComment(commentId);
 
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
