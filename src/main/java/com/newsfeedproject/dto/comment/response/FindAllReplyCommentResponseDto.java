dd779fec363350aa33a0a8288a6f889a2b57e246
answerin1 <dabin990723@naver.com>
2025-01-20 14:30:03 +0900

    댓글 관련 조회 및 대댓글 관련 조회 Dto 생성

---
+++ src/main/java/com/newsfeedproject/dto/comment/response/FindAllReplyCommentResponseDto.java
@@ -0,0 +1,18 @@
+package com.newsfeedproject.dto.comment.response;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FindAllReplyCommentResponseDto {
+	private Long id;
+	private Long parentId;
+	private String content;
+	private List<CreateCommentResponseDto> replies = new ArrayList<>();  // 대댓글(답글) 목록 추가
+	private LocalDateTime createdAt;
+}
